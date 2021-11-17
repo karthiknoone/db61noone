7884ae5c8bf5e7045f681b158ef7f53f1978726e
karthiknoone <karthiknoone@gmail.com>
2021-11-17 16:25:28 -0600

    updated the files

--- routes/tacobell.js
+++ routes/tacobell.js
@@ -2,19 +2,24 @@ var express = require('express');
 const tacobell_controlers = require('../controllers/tacobell');
 var router = express.Router();
 
-// /* GET home page. */
-// router.get('/', function (req, res, next) {
-//   res.render('tacobell', { title: 'Search Results for tacobell' });
-// });
+///* GET home page. */
+//router.get('/', function (req, res, next) {
+//  res.render('tacobell', { title: 'Search Results for tacobells' });
+//});
 
-/* GET tacobell */
+/* GET tacobells */
 router.get('/', tacobell_controlers.tacobell_view_all_Page);
-/* GET details with id of tacobell page */
-router.get('/tacobell/:id', tacobell_controlers.tacobell_detail);
 /* GET detail tacobell page */
 router.get('/detail', tacobell_controlers.tacobell_view_one_Page);
+/* GET create tacobell page */
+router.get('/create', tacobell_controlers.tacobell_create_Page);
+/* GET create update page */
+router.get('/update', tacobell_controlers.tacobell_update_Page);
+/* GET create delete page */
+router.get('/delete', tacobell_controlers.tacobell_delete_Page);
+/* GET details with id of tacobell page */
+router.get('/tacobell/:id', tacobell_controlers.tacobell_detail);
 /* DELETE details with id of tacobell page */
 router.get('/tacobell/:id', tacobell_controlers.tacobell_delete);
-
 
 module.exports = router;
